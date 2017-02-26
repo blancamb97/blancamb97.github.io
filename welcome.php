ec4cabbd190cf968ac5d3ca317f5b108fb074700
blancamb97 <bmartinburgos@smith.edu>
2017-02-26 10:37:24 -0500

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Title of the document</title>
+  	<link rel="stylesheet" href="styles.css">
+	</head>
+<body>
+
+Welcome <?php echo $_POST["username"]; ?><br>
+
+
+</body>
+</html>
